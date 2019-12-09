f0a25aa6f21300921671e6990f2407a5d70d3d7b
laalaunch <55928405+laalaunch@users.noreply.github.com>
2019-12-08 19:45:27 -0600

    Add files via upload

---
+++ hello-spring/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,46 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HelloController {
+
+    // Responds to /hello?name=LaunchCode
+    @RequestMapping(value ="hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        // The first attribute needs to match up with the ${} in the html.
+        // The second attribut is the object being passed in the String above.
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
+    // Responds to /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
+    @GetMapping("form")
+    public String helloForm() {
+        return "form";
+    }
+
+    // (Model model) passes the names variable to the hello-list template
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
+}
